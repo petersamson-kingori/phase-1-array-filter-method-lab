d6249eb399146e27e8ed1dba2cf2c5e9292cc99e
petersamson-kingori <peters.kingori@student.moringaschool.com>
2022-12-16 22:39:15 +0300

    commit tree 6fd985e41223c0eb96f9427af767a1e4d5a32cd3

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    string = string.toLowerCase();
+    const matchingDrivers = drivers.filter((driver) => {
+        const driverName = driver.toLowerCase();
+        return driverName.includes(string);
+      });
+    
+      return matchingDrivers;
+    }
+
+    function fuzzyMatch(drivers, string) {
+        string = string.toLowerCase();
+        const matchingDrivers = drivers.filter((driver) => {
+          const driverName = driver.toLowerCase();
+          return driverName.startsWith(string);
+        });
+      
+        return matchingDrivers;
+      }
+      
+
+      function matchName(drivers, string) {
+        string = string.toLowerCase();
+        const matchingDrivers = drivers.filter((driver) => {
+          const driverName = driver.name.toLowerCase();
+          return driverName.includes(string);
+        });
+      
+        return matchingDrivers;
+      }
+      
